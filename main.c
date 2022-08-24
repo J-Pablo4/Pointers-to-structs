5ea5fa68cd65a6154946f64ef210150527a7006b
Juan Pablo <pablo.lopez@iteso.mx>
2022-08-24 10:18:03 -0500

    con apuntadores struct y comparacion entre direcciones de memoria

--- main.c
+++ main.c
@@ -11,6 +11,8 @@ int main(void)
 {
     Vector_2d mi_vector_chilo = {5, 6.0};
 
+    printf("desde main: %p\n", &mi_vector_chilo);
+
     square_vector(&mi_vector_chilo);
 
     printf("%f %f", mi_vector_chilo.x, mi_vector_chilo.y);
@@ -19,6 +21,7 @@ int main(void)
 
 void square_vector(Vector_2d *vector)
 {
+    printf("desde square_vector: %p\n", vector);
     (*vector).x = (*vector).x * (*vector).x; //importante los parentesis para obtener el valor de dereferencia
 //    vector->x = vector->x * vector->x; Es una simplificacion de las referencias. Solo funciona a apuntadores de structs.
     (*vector).y = (*vector).y * (*vector).y;

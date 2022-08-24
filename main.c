f17f0034c9fa87da4680adb01e3ffa42a3e01fe4
Juan Pablo <pablo.lopez@iteso.mx>
2022-08-24 09:27:25 -0500

    sin apuntadores

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+void swap(int, int); //a <-> b
+
 int main() {
-    printf("Hello, World!\n");
+    int a_en_main = 12;
+    int b_en_main = -5;
+
+    swap(a_en_main,b_en_main);
+
+    printf("%d %d\n",a_en_main,b_en_main);
+
     return 0;
+}
+
+void swap(int a, int b)
+{
+    int pivote = a;
+    a = b;
+    b = pivote;
+
+    printf("%d %d\n", a,b);
+
 }

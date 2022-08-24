a8cb95df74e4669996a7ada7e40d78b415c41b15
Juan Pablo <pablo.lopez@iteso.mx>
2022-08-24 09:39:40 -0500

    con apuntadores y sin struct

--- main.c
+++ main.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
-void swap(int, int); //a <-> b
+void swap(int *, int *); //a <-> b
 
 int main() {
     int a_en_main = 12;
     int b_en_main = -5;
 
-    swap(a_en_main,b_en_main);
+    swap(&a_en_main,&b_en_main); //Hace referencia a las direcciones de memoria
 
     printf("%d %d\n",a_en_main,b_en_main);
 
     return 0;
 }
 
-void swap(int a, int b)
+void swap(int *a, int *b)
 {
-    int pivote = a;
-    a = b;
-    b = pivote;
-
-    printf("%d %d\n", a,b);
-
+    int pivote = *a; //Obtener valor de la direccion
+    *a = *b;//Obtener valor de la direccion
+    *b = pivote; //Obtener valor de la direccion
 }
